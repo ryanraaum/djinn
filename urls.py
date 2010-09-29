49997a047950418f87f2883b4c0261b455182afe
Ryan Raaum <github@raaum.org>
2010-09-29 18:29:02 -0400

    update target for contact url

--- urls.py
+++ urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     # (r'^djinn/', include('djinn.foo.urls')),
     url(r'^$', 'django.views.generic.simple.direct_to_template', 
         {'template':'base_index.html'}, name='base_index'),
-    url(r'^contact/$', lambda r: HttpResponseRedirect('http://www.raaum.org/contact'), name='contact'),
+    url(r'^contact/$', lambda r: HttpResponseRedirect('http://www.raaum.org'), name='contact'),
     (r'^transform/', include('djinn.mttransform.urls')),
     (r'^haplotype/', include('djinn.mthaplotype.urls')),
 
