2a5a6cc6752534ca5010eb571e54faa35ea88d88
Ryan Raaum <code@raaum.org>
2010-10-11 15:46:53 -0400

    switch from in-process matching to subprocess popen

--- mthaplotype/tasks.py
+++ mthaplotype/tasks.py
@@ -1,7 +1,9 @@
-from oldowan.mitotype import HVRMatcher
 from oldowan.mitotype import prevalidate_submission
 from celery.decorators import task
 from mthaplotype.models import Haplotyping
+
+import subprocess
+import tempfile
 
 #----------------------------------------------------------------------------#
 # PROCESSORS
@@ -16,18 +18,34 @@ def process_haplotypes(h_id):
         return False
     else:
         h = hlist[0]
-        print(h)
         h.completed = True
         in_data = h.in_data.encode('ascii', 'ignore')
-        print(in_data)
 
         vi = prevalidate_submission(in_data)
         if vi.valid:
-            HVRM = HVRMatcher()
-            results = HVRM.match(in_data, vi)
-            result_strings = [str(r) for r in results]
-            h.out_data = '\n'.join(result_strings)
-            h.success = True
+            infile = tempfile.NamedTemporaryFile()
+            outfile = tempfile.TemporaryFile()
+
+            infile.write(in_data)
+            infile.flush()
+
+            p = subprocess.Popen(['mitotype', '-f', infile.name], stdout=outfile)
+            exit_status = p.wait()
+
+            # rewind the outfile
+            outfile.seek(0)
+            results = outfile.read()
+
+            infile.close()
+            outfile.close()
+            
+            if exit_status == 0:
+                h.success = True
+                h.out_data = results
+            else:
+                h.success = False
+                h.problems = results
+
             h.save()
             return True
         else: # not valid
